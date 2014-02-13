89641062380672856181610264fd5bc128ffbd0d
achoraev <audulin@gmail.com>
2014-02-13 20:32:12 +0200

    13.02.2014

---
+++ TeamJosephHeller/Game/GameException.cs
@@ -0,0 +1,22 @@
+﻿namespace Game
+{
+    using System;
+
+    public class GameException: ApplicationException
+    {
+        //field
+        public string ExMessage { get; set; }
+        //construktors
+        public GameException(string ExMessage)
+        :base(ExMessage)
+        {
+            
+        }
+
+        public GameException(string ExMessage, Exception innerEx)
+            : base(ExMessage, innerEx)
+        {
+
+        }
+    }
+}

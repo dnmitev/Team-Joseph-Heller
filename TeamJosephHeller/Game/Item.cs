22cb7e8b52aa5416a134015c44a8448ebf8fb1a9
Dimitar Mitev <dnmitev@gmail.com>
2014-02-23 12:22:56 +0200

    Item main class added

---
+++ TeamJosephHeller/Game/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NinjaWars
+{
+    public class Item : MovingObject
+    {
+
+        public override void RespondToCollision(Interfaces.ICollidable collideWith)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
